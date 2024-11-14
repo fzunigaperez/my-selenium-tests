fd45fb9cec67790e28b31c148e2a5ad36f752329
fzunigaperez <44201451+fzunigaperez@users.noreply.github.com>
2024-11-14 14:52:25 +0100

    Update my-selenium-tests.js

--- my-selenium-tests.js
+++ my-selenium-tests.js
@@ -1,11 +1,22 @@
 const { Builder, By, Key, until } = require('selenium-webdriver');
 const browserstack = require('browserstack-local');
 
+// Configuración de capacidades para BrowserStack
+const capabilities = {
+  'browserstack.user': 'fzuniga_kU2wfa', // Reemplázalo con tu nombre de usuario de BrowserStack
+  'browserstack.key': 'PDSWH3QTgRyFqVWFR1sx', // Reemplázalo con tu clave de acceso de BrowserStack
+  'browser': 'Chrome', // El navegador que deseas usar
+  'browser_version': 'latest', // Versión del navegador
+  'os': 'Windows', // Sistema operativo en el que se ejecuta el navegador
+  'os_version': '10', // Versión del sistema operativo
+  'name': 'Google Search Test', // Nombre del test en BrowserStack
+  'build': 'Build 1', // Nombre del build (opcional)
+  'browserstack.local': 'false', // Si no estás probando en un entorno local
+  'browserstack.debug': 'true', // Habilitar los registros de depuración
+};
+
 // Función asíncrona para ejecutar los tests
 async function runTest() {
-  
-  
-
   // Inicializa el driver de Selenium con las capacidades de BrowserStack
   let driver = await new Builder()
     .usingServer('https://hub-cloud.browserstack.com/wd/hub') // URL de BrowserStack
@@ -31,4 +42,3 @@ async function runTest() {
 
 // Ejecuta la función asíncrona
 runTest();
-
