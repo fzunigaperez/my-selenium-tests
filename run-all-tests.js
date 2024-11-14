4740b1b9c193b1415bd4753482ea9cd0a6bcc2a7
fzunigaperez <44201451+fzunigaperez@users.noreply.github.com>
2024-11-14 15:20:15 +0100

    Update run-all-tests.js

--- run-all-tests.js
+++ run-all-tests.js
@@ -1,13 +1,13 @@
 // run-all-tests.js
 
-const runTest1 = require('./my-selenium-tests.js');
-const runTest2 = require('./my-second-selenium-test.js');
-const runTest3 = require('./my-third-selenium-test.js');
+const runTest1 = require('./google');
+const runTest2 = require('.capoeira');
+
 
 async function runAllTests() {
   await runTest1();
   await runTest2();
-  await runTest3();
+  
 }
 
 runAllTests();
