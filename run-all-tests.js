62bab3023a22818a9e9bce9f59f651d0d3fdd2c4
fzunigaperez <44201451+fzunigaperez@users.noreply.github.com>
2024-11-18 11:36:51 +0100

    Update run-all-tests.js

--- run-all-tests.js
+++ run-all-tests.js
@@ -1,12 +1,14 @@
 // run-all-tests.js
 
-const C178 = require('./testCases/C178');   
-const C180 = require('./testCases/google');
+//const C178 = require('./testCases/C178');   
+//const C180 = require('./testCases/google');
+const C90 = require('./testCases/google');
 
 
 async function runAllTests() {
-  await C178();
-  await C180();
+  //await C178();
+  //await C180();
+  await C90();
   
 }
 
