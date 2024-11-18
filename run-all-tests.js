ed2ce624f9604fea72af332a70be6cdab1a7d73e
Fernando <fzuniga@phoenixcontact-sb.io>
2024-11-18 15:32:22 +0100

    C90 new added with the shared function aproach

--- run-all-tests.js
+++ run-all-tests.js
@@ -2,14 +2,14 @@
 
 //const C178 = require('./testCases/C178');   
 //const C180 = require('./testCases/google');
-const C90 = require('./testCases/C90');  //the route has to include the name of the file
-
+const C90new = require('./testCases/C90new');  //the route has to include the name of the file
+const C90 = require('./testCases/C90'); 
 
 async function runAllTests() {
   //await C178();
   //await C180();
+  await C90new();
   await C90();
-  
 }
 
 runAllTests();
