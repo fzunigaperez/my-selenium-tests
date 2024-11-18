237eb04b349942dcbe2f1c2d52bcd5ee54e8327b
Fernando <fzuniga@phoenixcontact-sb.io>
2024-11-18 16:27:51 +0100

    C15 and C90 same logic

--- run-all-tests.js
+++ run-all-tests.js
@@ -3,14 +3,14 @@
 //const C178 = require('./testCases/C178');   
 //const C180 = require('./testCases/google');
 //const C90new = require('./testCases/C90new');  //the route has to include the name of the file
-//const C90 = require('./testCases/C90'); 
+const C90 = require('./testCases/C90'); 
 const C15 = require('./testCases/C15'); 
 
 async function runAllTests() {
   //await C178();
   //await C180();
  // await C90new();
-  //await C90();
+  await C90();
   await C15();
 }
 
