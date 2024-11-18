746304fdaeaa2447961df10cf28919ea21b4bef8
fzunigaperez <44201451+fzunigaperez@users.noreply.github.com>
2024-11-18 16:01:15 +0100

    c15 added

--- run-all-tests.js
+++ run-all-tests.js
@@ -2,14 +2,16 @@
 
 //const C178 = require('./testCases/C178');   
 //const C180 = require('./testCases/google');
-const C90new = require('./testCases/C90new');  //the route has to include the name of the file
-const C90 = require('./testCases/C90'); 
+//const C90new = require('./testCases/C90new');  //the route has to include the name of the file
+//const C90 = require('./testCases/C90'); 
+const C15 = require('./testCases/C15'); 
 
 async function runAllTests() {
   //await C178();
   //await C180();
-  await C90new();
-  await C90();
+ // await C90new();
+  //await C90();
+  await C15();
 }
 
 runAllTests();
