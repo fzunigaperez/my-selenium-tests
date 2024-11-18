141948d5770927aecfd853e17b1bbcad94bc5a82
Fernando <fzuniga@phoenixcontact-sb.io>
2024-11-18 18:14:13 +0100

    improvement for test failing not causing problems

--- run-all-tests.js
+++ run-all-tests.js
@@ -1,16 +1,29 @@
-// run-all-tests.js
+const C90 = require('./testCases/C90');
+const C15 = require('./testCases/C15');
+// Agrega aquí más tests según sea necesario.
 
-//const C178 = require('./testCases/C178');   
-//const C180 = require('./testCases/google');
-//const C90new = require('./testCases/C90new');  //the route has to include the name of the file
-const C90 = require('./testCases/C90'); 
-const C15 = require('./testCases/C15'); 
+async function runTest(testFunction, testName) {
+  try {
+    console.log(`Running test: ${testName}`);
+    await testFunction();
+    console.log(`${testName} completed successfully.`);
+  } catch (error) {
+    console.error(`${testName} failed:`, error.message);
+  }
+}
 
 async function runAllTests() {
-  
-  await C15();
-  await C90();
-  
+  const tests = [
+    { name: 'C90', func: C90 },
+    { name: 'C15', func: C15 },
+    // Agrega aquí más tests según sea necesario.
+  ];
+
+  for (const test of tests) {
+    await runTest(test.func, test.name);
+  }
+
+  console.log('All tests have been executed.');
 }
 
 runAllTests();
