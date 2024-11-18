ebabfb3c0b80cd638e0e959a9ca28910b05c2b6d
fzunigaperez <44201451+fzunigaperez@users.noreply.github.com>
2024-11-18 11:40:37 +0100

    Update run-all-tests.js

--- run-all-tests.js
+++ run-all-tests.js
@@ -2,7 +2,7 @@
 
 //const C178 = require('./testCases/C178');   
 //const C180 = require('./testCases/google');
-const C90 = require('./testCases/google');
+const C90 = require('./testCases/C90');  //the route has to include the name of the file
 
 
 async function runAllTests() {
