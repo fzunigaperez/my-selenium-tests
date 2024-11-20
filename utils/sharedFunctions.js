2af38c35758b39eeed3e55533f40a45bce30f5cb
Fernando <fzuniga@phoenixcontact-sb.io>
2024-11-20 19:56:32 +0100

    delete email function added

--- utils/sharedFunctions.js
+++ utils/sharedFunctions.js
@@ -199,12 +199,60 @@ async function loginViewer(driver, vars) {
   
   async function deleteAllEmails(driver) {
     console.log("Deleting all mails...");
-    await driver.findElement(By.xpath("//span[contains(text(),'All mail')]")).click();
+    // Esperar que se localice "Inbox"
+    await driver.wait(until.elementLocated(By.xpath("//span[contains(text(),'Inbox')]")), 60000);
+
+    // Comprobar si existe "Less"
+    const lessElements = await driver.findElements(By.xpath("//span[normalize-space()='Less']"));
+    if (lessElements.length > 0) {
+        await driver.sleep(2000);
+    } else {
+        // Hacer clic en "More" si "Less" no está visible
+        const moreButton = await driver.findElement(By.xpath("//span[normalize-space()='More']"));
+        await moreButton.click();
+        await driver.sleep(2000);
+    }
+
+    // Hacer clic en "All mail"
+    const allMailButton = await driver.findElement(By.xpath("//span[contains(text(),'All mail')]"));
+    await allMailButton.click();
     await driver.sleep(2000);
+
+    // Contar elementos
+    const mailItems = await driver.findElements(By.xpath("//div[2]/div[2]/div/div"));
+    console.log(`Número de correos: ${mailItems.length}`);
+
+    // Seleccionar todos los correos
+    await driver.wait(until.elementLocated(By.id("idSelectAll")), 30000);
+    const selectAllButton = await driver.findElement(By.id("idSelectAll"));
+    await selectAllButton.click();
+    await driver.sleep(2000);
+
+    // Mover a la papelera
+    await driver.wait(until.elementLocated(By.xpath("//button[contains(.,'Move to trash')]")), 30000);
+    const moveToTrashButton = await driver.findElement(By.xpath("//button[contains(.,'Move to trash')]"));
+    await moveToTrashButton.click();
+    await driver.sleep(2000);
+
+    // Navegar a la papelera
+    const trashButton = await driver.findElement(By.xpath("//span[@class='text-ellipsis'][contains(.,'Trash')]"));
+    await trashButton.click();
+    await driver.sleep(2000);
+
+    // Seleccionar todo en la papelera
     await driver.findElement(By.id("idSelectAll")).click();
-    await driver.findElement(By.xpath("//button[contains(text(),'Move to trash')]")).click();
     await driver.sleep(2000);
-    console.log("All mails moved to trash.");
+
+    // Eliminar permanentemente
+    const deletePermanentlyButton = await driver.findElement(By.xpath("//button[contains(.,'Delete permanently')]"));
+    await deletePermanentlyButton.click();
+    await driver.sleep(2000);
+
+    // Confirmar eliminación
+    const confirmDeleteButton = await driver.findElement(By.xpath("//button[contains(text(),'Delete')]"));
+    await confirmDeleteButton.click();
+    await driver.sleep(2000);
+   
   }
   
 
