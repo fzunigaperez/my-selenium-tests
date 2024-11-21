de1c66f05acebb589fb4ca614245a1ea1d23a611
Fernando <fzuniga@phoenixcontact-sb.io>
2024-11-21 15:12:13 +0100

    test

--- testCases/C90.js
+++ testCases/C90.js
@@ -23,3 +23,15 @@ async function C90() {
 module.exports = C90;
 
 
+if (require.main === module) {
+  (async () => {
+    try {
+      console.log(`'🚀 Ejecutando el test `);
+      await C90();   // Change here the test name
+      
+      console.log('✅ Test completado con éxito.');
+    } catch (error) {
+      console.error('❌ Error al ejecutar el test:', error.message);
+    }
+  })();
+}
