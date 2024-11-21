934b4baeb23b1e4b3a39d7614c6251f29f962e91
Fernando <fzuniga@phoenixcontact-sb.io>
2024-11-21 10:10:06 +0100

    calling vars

--- testCases/C90.js
+++ testCases/C90.js
@@ -7,7 +7,7 @@ async function C90() {
     await windowConfiguration(driver);
 
     // Inicio de sesión
-    await loginAdmin(driver);
+    await loginAdmin(driver,vars);
 
     // Cierre de sesión
     await logout(driver);
