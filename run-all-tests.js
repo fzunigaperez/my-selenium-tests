30431f12dfe11a60462debbb729304e8701198b6
Fernando <fzuniga@phoenixcontact-sb.io>
2024-11-26 16:35:28 +0100

    added more test cases

--- run-all-tests.js
+++ run-all-tests.js
@@ -1,6 +1,15 @@
 const { sendResultToTestRail, createTestRun } = require('./utils/sharedFunctions'); // Importa createTestRun
-const C90 = require('./testCases/C90');
+
 const C15 = require('./testCases/C15');
+const C655 = require('./testCases/C655');
+const C656 = require('./testCases/C656');
+const C16 = require('./testCases/C16');
+const C18 = require('./testCases/C18'); 
+const C36 = require('./testCases/C36'); 
+const C580 = require('./testCases/C580');
+
+const C90 = require('./testCases/C90');
+
 
 // Función para extraer el testCaseId de sessionName
 function extractTestCaseId(sessionName) {
@@ -11,7 +20,14 @@ function extractTestCaseId(sessionName) {
 // Construcción del array tests con el formato original
 const sessionNames = [
   { name: 'C90_Log out successfully', func: C90 },
-  { name: 'C15_Log out successfully', func: C15 },
+  { name: 'C15_Login with right credentials as ADMIN', func: C15 },
+  { name: 'C655_Login with right credentials as EDITOR', func: C655 },
+  { name: 'C656_Login with right credentials as VIEWER', func: C656 },
+  { name: 'C16_Login with wrong credentials', func: C16 },
+  { name: 'C18_Login with valid email but wrong password', func: C18 },
+  { name: 'C36_Login with wrong credentials (10 wrong attempts)', func: C36 },
+  { name: 'C580_Password forgotten', func: C580 }
+  
 ];
 
 const tests = sessionNames.map(({ name, func }) => ({
