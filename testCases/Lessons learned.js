9c253832f15e88bdc1e9bc69ad7e254285cde0e1
Fernando <fzuniga@phoenixcontact-sb.io>
2024-11-26 14:35:56 +0100

    test

--- testCases/Lessons learned.js
+++ testCases/Lessons learned.js
@@ -1,6 +1,6 @@
 
 
-// When it is necessary to wait when a new page loadss
+// When it is necessary to wait when a new page loadssw
 
 await driver.sleep(1000);
 await driver.wait(until.elementLocated(By.id("username")), 30000);
