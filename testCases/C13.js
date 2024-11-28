8161b8322d5e80b32c773192456a61619b6f6dab
Fernando <fzuniga@phoenixcontact-sb.io>
2024-11-28 16:45:59 +0100

    C13 working?

--- testCases/C13.js
+++ testCases/C13.js
@@ -4,19 +4,14 @@ const assert = require('assert');
 const testBase = require('./testBase'); // Lógica común para la ejecución de pruebas
 const {
   windowConfiguration,
-  logout,
-  loginToProtonMail,
-  logOutFromProtonMail,
-  confirmLinkURLsOn,
-  loginUnregisteredUser,
-  unregisteredUserCredentials,
   deleteUnregisteredUserInCaseOfExistence,
-  agreeTerms,
-  waitUntilXpathNotPresent,
-  loginLandingPageButton,
-  acceptCookies,
-  waitingLoadingRingProficloudToDissapear,
   enterRegistrationData,
+  emailVerification,
+  loginAsUnregisteredUserAndDeleteAccount,
+  waitingLoadingRingProficloudToDissapear,
+  loginToProtonMail,
+  deleteAllEmails,
+  logOutFromProtonMail,
 } = require('../utils/sharedFunctions'); // Importación de funciones reutilizables
 
 async function C13() {
@@ -26,74 +21,39 @@ async function C13() {
       async (driver) => {
         let vars = {};
 
-        /*await windowConfiguration(driver);
-        await acceptCookies(driver);
-        await loginToProtonMail(driver,vars);
-        await confirmLinkURLsOn(driver,vars);
-        await logOutFromProtonMail(driver,vars);*/
-        //await windowConfiguration(driver);
-        
-  
+             
         await deleteUnregisteredUserInCaseOfExistence(driver,vars);
         
-
-
         await windowConfiguration(driver);
-        await acceptCookies(driver);
-        //await loginLandingPageButton(driver);
-
         await driver.wait(until.elementLocated(By.id("registration-button")), 30000).click();
-        await enterRegistrationData (driver, vars);
-
-        //REGISTRATION
-
-        // C895 If the user enter an invalid email, and correct it later, it is should be possible to REGISTER to Proficloud or create Billing account.
-
-       // await driver.findElement(By.id("registration-button")).click();
+        await enterRegistrationData(driver, vars);
+        //Because is the first time happening we wait for the success message to dissapear, since this message does it by itself
+        await waitingLoadingRingProficloudToDissapear(driver,vars);
         
-         /* await driver.findElement(By.xpath("//input[@placeholder=\'Organization name\']")).sendKeys("Unregistered Orga");
-          await driver.findElement(By.xpath("//input[@placeholder=\'Email\']")).sendKeys("thisEmailNotValid@");
-          await driver.findElement(By.xpath("//div[@class=\'title\'][contains(.,\'Registration\')]")).click();
-          await driver.sleep(1000);
-        
-          await driver.findElements(By.xpath("//app-icon[@name=\'warning\']//*[name()=\'svg\']//*[name()=\'path\' and contains(@class,\'ng-star-in\')]"));
-            
-          await driver.findElement(By.xpath("//input[@placeholder=\'Email\']")).clear();
-          await driver.findElement(By.xpath("//input[@placeholder=\'Email\']")).sendKeys(vars["username"]);
-          await driver.findElement(By.id("mat-select-value-1")).click();
-          await driver.findElement(By.xpath("//span[contains(.,\'Spain\')]")).click();
-          await driver.findElement(By.xpath("//input[@placeholder=\'First name\']")).sendKeys(vars["firstName"]);
-          await driver.findElement(By.xpath("//input[@placeholder=\'Last name\']")).sendKeys(vars["lastName"]);
-          await driver.findElement(By.xpath("//input[@placeholder=\'Password\']")).sendKeys(vars["password"]);
-          await driver.findElement(By.xpath("//input[contains(@placeholder,\'Confirm password\')]")).sendKeys(vars["password"]);
-          await driver.sleep(1000);
+        await driver.wait(until.elementLocated(By.xpath("//span[contains(.,'Register')]")), 2000).click();
+        await enterRegistrationData(driver, vars);
+        //await driver.sleep(10000);
 
-
-          // C1024  Check and uncheck the Terms and Licences Agreement should not alter the registered button
-          await driver.findElements(By.xpath("//*[@disabled=\'true\'][contains(.,\'Register\')]"));
-
-          // Agree terms
-          await agreeTerms(driver);
-          //Assert that Register button is deactivated
-          const buttonRegisterDisabled = await driver.findElements(By.xpath("//*[@disabled=\'true\'][contains(.,\'Register\')]"));
-          assert(!buttonRegisterDisabled.length);
-          await agreeTerms(driver);
-          await driver.findElements(By.xpath("//*[@disabled=\'true\'][contains(.,\'Register\')]"));
-          await agreeTerms(driver);
-          assert(!buttonRegisterDisabled.length);
-          await driver.findElement(By.xpath("//span[contains(.,\'Register\')]")).click();*/
-
-
-          // Waiting the loading Ring to dissapear
-          await waitingLoadingRingProficloudToDissapear (driver);
-
-
-          
 
        
+        await driver.wait(until.elementLocated(By.xpath("//span[contains(.,'You are already registered. Please check the inbox of your given email address. If you experience any trouble, please contact our support.')]")), 30000);
+        await driver.wait(until.elementLocated(By.id("modal-close")), 5000).click();
+        //await driver.wait(until.elementLocated(By.xpath("//span[contains(.,'Register')]")), 2000).click();
 
-        //await loginAdmin(driver, vars);
-        //await logout(driver);
+
+        await emailVerification(driver,vars);
+        await loginAsUnregisteredUserAndDeleteAccount(driver,vars);
+        await loginToProtonMail(driver,vars);
+        await deleteAllEmails(driver,vars);
+        await logOutFromProtonMail(driver);
+
+
+
+
+        
+
+
+
       }
     );
   } catch (error) {
