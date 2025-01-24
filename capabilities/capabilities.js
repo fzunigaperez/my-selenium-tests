6479d0d4d0d152162b66d990af8b40dc0da29a9e
Fernando <fzuniga@phoenixcontact-sb.io>
2025-01-24 15:55:19 +0100

    V3

--- capabilities/capabilities.js
+++ capabilities/capabilities.js
@@ -2,22 +2,22 @@ module.exports = {
   'bstack:options': {
     'os': 'Windows',
     'osVersion': '10',
-    'local': 'false', // Habilitar BrowserStack Local para redirigir descargas a tu máquina local
-    'seleniumVersion': '4.21.0', // Asegúrate de usar una versión compatible
+    'local': 'false',
+    'seleniumVersion': '4.21.0',
     'userName': process.env.BROWSERSTACK_USERNAME,
     'accessKey': process.env.BROWSERSTACK_ACCESS_KEY,
-    'idleTimeout': 90, // Tiempo de espera inactivo configurado a 90 segundos
-    'console': 'verbose', // Capturar todos los logs de la consola
-    'networkLogs': true, // Capturar logs de red
-    'debug': true, // Activar la grabación visual
+    'idleTimeout': 90,
+    'networkLogs': true, // Captura logs de red
+    'debug': true, // Activar depuración
   },
+  'browserstack.console': 'verbose', // Mover fuera de 'bstack:options'
   'browserName': 'Chrome',
   'browserVersion': 'latest',
   'goog:chromeOptions': {
     prefs: {
-      'download.prompt_for_download': false, // Deshabilitar los diálogos de descarga
-      'download.directory_upgrade': true, // Permitir cambios automáticos en la carpeta de descarga
-      'safebrowsing.enabled': true, // Activar la protección contra contenido inseguro
-    }
-  }
+      'download.prompt_for_download': false,
+      'download.directory_upgrade': true,
+      'safebrowsing.enabled': true,
+    },
+  },
 };
